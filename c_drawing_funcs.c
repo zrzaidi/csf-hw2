3832d5f5a0865f147e11b07640ca10a5aac68375
Zayn Zaidi <112359225+zrzaidi@users.noreply.github.com>
2024-02-14 23:59:03 -0500

    Add files via upload

--- c_drawing_funcs.c
+++ c_drawing_funcs.c
@@ -17,13 +17,13 @@ uint8_t get_r(uint32_t color) {
   return shifted & 0xFF;
 }
 
-uint8_t get_b(uint32_t color) {
-  uint32_t shifted = color >> 8;
+uint8_t get_g(uint32_t color) {
+  uint32_t shifted = color >> 16;
   return shifted & 0xFF;
 }
 
-uint8_t get_g(uint32_t color) {
-  uint32_t shifted = color >> 16;
+uint8_t get_b(uint32_t color) {
+  uint32_t shifted = color >> 8;
   return shifted & 0xFF;
 }
 
@@ -39,12 +39,8 @@ bool in_bounds(struct Image *img, int32_t x, int32_t y) {
 }
 
 uint32_t compute_index(struct Image *img, int32_t x, int32_t y) {
-  return img->width * x + y;
+  return img->width * y + x;
 }
-
-// uint32_t r_blend(struct Image *img, int32_t x, int32_t y, uint32_t color) {
-  
-// }
 
 ////////////////////////////////////////////////////////////////////////
 // API functions
@@ -68,10 +64,15 @@ void draw_pixel(struct Image *img, int32_t x, int32_t y, uint32_t color) {
   }
   uint32_t background = img->data[index];
   uint32_t newColor = 0;
-  newColor = ((get_a(color)*get_r(color) + (255 - get_a(color)) * get_r(background)) / 255) << 8;
-  newColor = (newColor | ((get_a(color)*get_g(color) + (255 - get_a(color)) * get_g(background)) / 255)) << 8;
-  newColor = (newColor | ((get_a(color)*get_b(color) + (255 - get_a(color)) * get_b(background)) / 255)) << 8;
-  newColor = newColor | 255;
+  if (get_a(color) == 255) {
+    newColor = color;
+  } else {
+    newColor = ((get_a(color)*get_r(color) + (255 - get_a(color)) * get_r(background)) / 255) << 24;
+    newColor = newColor | ((get_a(color)*get_g(color) + (255 - get_a(color)) * get_g(background)) / 255) << 16;
+    newColor = newColor | ((get_a(color)*get_b(color) + (255 - get_a(color)) * get_b(background)) / 255) << 8;
+    newColor = newColor | 255;
+  }
+  //std::cout<<newColor<<std::endl;
   img->data[index] = newColor;
 }
 
@@ -92,7 +93,7 @@ void draw_rect(struct Image *img,
   for(uint32_t i = 0; i < img->width; i++){
     for(uint32_t j = 0; j < img->height; j++){
       if ((j >= rect->y) && (j < (rect->y + rect->height)) && (i >= rect->x) && (i < (rect->x + rect->width))) {
-        draw_pixel(img, j, i, color);
+        draw_pixel(img, i, j, color);
       }
     }
   }
@@ -116,7 +117,7 @@ void draw_circle(struct Image *img,
     for(uint32_t j = 0; j < img->height; j++){
       uint32_t rad = (x - i)*(x - i) + (y - j)*(y - j);
       if (rad <= r*r) {
-        draw_pixel(img, j, i, color);
+        draw_pixel(img, i, j, color);
       }
     }
   }
